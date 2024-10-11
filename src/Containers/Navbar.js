c85aa69791b74ea2b519786b2b3d1842586df31b
satyanagkumar <nagendra1283360@gmail.com>
2024-10-11 12:46:35 -0500

    Changes added for styling

--- src/Containers/Navbar.js
+++ src/Containers/Navbar.js
@@ -4,21 +4,23 @@ import { NavLink } from 'react-router-dom';
 function Navbar() {
     return (
         <nav className="navbar">
-            <NavLink className="navbar-brand" to="/">Web of Things</NavLink>
-            <ul className="navbar-nav">
-                <li>
-                    <NavLink className="nav-link" to="/" exact activeClassName="active">Home</NavLink>
-                </li>
-                <li>
-                    <NavLink className="nav-link" to="/Content" activeClassName="active">Content</NavLink>
-                </li>
-                <li>
-                    <NavLink className="nav-link" to="/Taxonomy" activeClassName="active">Taxonomy</NavLink>
-                </li>
-                <li>
-                    <NavLink className="nav-link" to="/Team" activeClassName="active">Team</NavLink>
-                </li>
-            </ul>
+            <div className="nav-container">
+                <NavLink className="navbar-brand" to="/">Web of Things</NavLink>
+                <ul className="navbar-nav">
+                    <li>
+                        <NavLink className="nav-link" to="/" exact activeClassName="active">Home</NavLink>
+                    </li>
+                    <li>
+                        <NavLink className="nav-link" to="/Content" activeClassName="active">Content</NavLink>
+                    </li>
+                    <li>
+                        <NavLink className="nav-link" to="/Taxonomy" activeClassName="active">Taxonomy</NavLink>
+                    </li>
+                    <li>
+                        <NavLink className="nav-link" to="/Team" activeClassName="active">Team</NavLink>
+                    </li>
+                </ul>
+            </div>
         </nav>
     );
 }
