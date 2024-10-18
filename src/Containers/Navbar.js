f5f272894820fbe30efa9d836433ed1a4f2dc34b
rmusukudabbidi <rmusukudabbidi@slu.edu>
2024-10-18 10:38:55 -0500

    mobile view

--- src/Containers/Navbar.js
+++ src/Containers/Navbar.js
@@ -1,23 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import '../styles/Navbar.css'; // Ensure the path is correct
 
 function Navbar() {
+    const [isMobile, setIsMobile] = useState(false);
+
+    const handleToggle = () => {
+        setIsMobile(!isMobile);
+    };
+
     return (
         <nav className="navbar">
             <div className="nav-container">
                 <NavLink className="navbar-brand" to="/">Web of Things</NavLink>
-                <ul className="navbar-nav">
+                <div className="hamburger-icon" onClick={handleToggle}>
+                    <span className="bar"></span>
+                    <span className="bar"></span>
+                    <span className="bar"></span>
+                </div>
+                <ul className={`navbar-nav ${isMobile ? "nav-active" : ""}`}>
                     <li>
-                        <NavLink className="nav-link" to="/" exact activeClassName="active">Home</NavLink>
+                        <NavLink className="nav-link" to="/" exact activeClassName="active" onClick={handleToggle}>Home</NavLink>
                     </li>
                     <li>
-                        <NavLink className="nav-link" to="/Content" activeClassName="active">Content</NavLink>
+                        <NavLink className="nav-link" to="/Content" activeClassName="active" onClick={handleToggle}>Content</NavLink>
                     </li>
                     <li>
-                        <NavLink className="nav-link" to="/Taxonomy" activeClassName="active">Taxonomy</NavLink>
+                        <NavLink className="nav-link" to="/Taxonomy" activeClassName="active" onClick={handleToggle}>Taxonomy</NavLink>
                     </li>
                     <li>
-                        <NavLink className="nav-link" to="/Team" activeClassName="active">Team</NavLink>
+                        <NavLink className="nav-link" to="/Team" activeClassName="active" onClick={handleToggle}>Team</NavLink>
                     </li>
                 </ul>
             </div>
