8d0b4fb4b010c67a2ae7027a30e7cf529e573c31
satyanagkumar <nagendra1283360@gmail.com>
2024-10-21 15:39:59 -0500

    Changes added

--- src/Containers/Home.js
+++ src/Containers/Home.js
@@ -1,24 +1,68 @@
 import React, { useState } from 'react';
+import pic9 from "./IOT Security Issues.png"; 
+import pic10 from "./connect.png";
+import pic11 from "./IOT_overview.jpg" ;
+import pic12 from "./MI Integration.png" // Ensure the image path is correct
 
 function Home() {
-  const [activeSection, setActiveSection] = useState('challenges');
+  const [activeSection, setActiveSection] = useState('Abstract');
 
   const sections = {
     Abstract:{
       title: "Abstract",
-      content:"The Web of Things (WoT) promises to seamlessly integrate numerous devices onto the Internet, transforming everyday objects into smart, interconnected components. This survey goes deeply into the systems and infrastructure that support WoT, investigating the field's intricacies, innovations, and future potential. This review examines recent research breakthroughs in IoT and WoT studies, focusing on five main areas: security, device management, interoperability, machine learning applications, and specialized architectures. Security studies address weaknesses like as hacking and data breaches by employing a variety of remedies such as encryption and machine learning-based anomaly detection. The papers on device management present a framework for discovering and controlling IoT devices based on Thing Descriptions (TDs). Interoperability research focuses on the use of web standards and middleware to address fragmentation concerns. Machine learning approaches, such as reinforcement learning, seek on optimization. Application-specific research focuses on health monitoring structures and search engines. The current survey summarizes approaches and issues while also providing insights for future research."
-     },
+      content: (
+        <>
+        <p>The Web of Things (WoT) promises to seamlessly integrate numerous devices onto the Internet, transforming everyday objects into smart, interconnected components. This survey goes deeply into the systems and infrastructure that support WoT, investigating the field's intricacies, innovations, and future potential. This review examines recent research breakthroughs in IoT and WoT studies, focusing on five main areas: security, device management, interoperability, machine learning applications, and specialized architectures. Security studies address weaknesses like as hacking and data breaches by employing a variety of remedies such as encryption and machine learning-based anomaly detection. The papers on device management present a framework for discovering and controlling IoT devices based on Thing Descriptions (TDs). Interoperability research focuses on the use of web standards and middleware to address fragmentation concerns. Machine learning approaches, such as reinforcement learning, seek on optimization. Application-specific research focuses on health monitoring structures and search engines. The current survey summarizes approaches and issues while also providing insights for future research.</p>
+        <div style={{ textAlign: "center" }}>
+          <img src={pic10} alt="Number of Devices Connected over years" style={{width: "100%", maxWidth: "400px"}} />
+          <p>Devices Connected in billion from the year 2015 to 2025</p>
+       </div>
+      </>
+
+      )
+    },
     challenges: {
       title: "Key Challenges in the Field",
-      content: "The rapidly increasing development of IoT devices presents a number of key issues in the industry. First and foremost, security and privacy concerns must be addressed; as the number of networked devices increases, so does the risk of security breaches and privacy violations. This risk is heightened by the variety and capabilities of the devices involved. Second, interoperability and standardization are significant challenges; without uniform standards across platforms and devices, efficient integration and functioning cannot be realized. Third, scaling and managing an expanding network of devices provide significant issues, particularly in preserving performance as the system grows. Finally, energy efficiency is a crucial issue; the increasing number of devices connecting to the Internet greatly rises energy demand, emphasizing the urgent need for more sustainable solutions.These problems not only illustrate the difficulty of integrating IoT into our daily lives, but they also identify crucial areas that must be addressed in order to fully realize the potential of IoT technologies."
+      content: (
+        <>
+          <p>The rapidly increasing development of IoT devices presents a number of key issues in the industry. First and foremost, security and privacy concerns must be addressed; as the number of networked devices increases, so does the risk of security breaches and privacy violations. This risk is heightened by the variety and capabilities of the devices involved. Second, interoperability and standardization are significant challenges; without uniform standards across platforms and devices, efficient integration and functioning cannot be realized. Third, scaling and managing an expanding network of devices provide significant issues, particularly in preserving performance as the system grows. Finally, energy efficiency is a crucial issue; the increasing number of devices connecting to the Internet greatly rises energy demand, emphasizing the urgent need for more sustainable solutions.These problems not only illustrate the difficulty of integrating IoT into our daily lives, but they also identify crucial areas that must be addressed in order to fully realize the potential of IoT technologies
+          </p>
+          <div style={{ textAlign: "center" }}>
+          <img src={pic9} alt="Challenges in IoT" style={{width: "100%", maxWidth: "400px"}} />
+          <p>Image shows the types of security challenges in IOT</p>
+       
+       </div>
+        </>
+      )
     },
     trends: {
       title: "Recent Trends and Advancements",
-      content: "Innovations of the Web of Things ‘WoT’ are defining its future with advances in addressing current issues and proliferation of opportunities. Edge and Fog Computing are revolutionary Key technologies which help to decrease latency and increase overall response times through data computation near the source of the said data. The decentralisation also allows for better control of the data traffic and increases signalling and processing time. Further, AI & ML integration is enhancing many IoT devices to be more intelligent for making efficient and intelligent decisions at its own. This integration enables the engineering of more sophisticated systems that execute intelligent and proactive activities, as well as reactive actions. Moreover, application of Blockchain Technology is gaining significance in providing a security to IoT transactions and data exchanges to build up trust. Its architecture is effective in protecting data and providing transparency in all the transactions. Finally, Semantic Technologies are undergoing application in device interconnectivity and data harmonization. The existent technologies offer a system structure that fosters the comprehension and cooperation between different systems and makes the WoT more secure and universal. Altogether, these trends and demands, indeed, responses to the WoT current problems as well as set the stage for further, more enhanced evolution of technologies there."
+      content: (
+        <>
+        <p>Innovations of the Web of Things ‘WoT’ are defining its future with advances in addressing current issues and proliferation of opportunities. Edge and Fog Computing are revolutionary Key technologies which help to decrease latency and increase overall response times through data computation near the source of the said data. The decentralisation also allows for better control of the data traffic and increases signalling and processing time. Further, AI & ML integration is enhancing many IoT devices to be more intelligent for making efficient and intelligent decisions at its own. This integration enables the engineering of more sophisticated systems that execute intelligent and proactive activities, as well as reactive actions. Moreover, application of Blockchain Technology is gaining significance in providing a security to IoT transactions and data exchanges to build up trust. Its architecture is effective in protecting data and providing transparency in all the transactions. Finally, Semantic Technologies are undergoing application in device interconnectivity and data harmonization. The existent technologies offer a system structure that fosters the comprehension and cooperation between different systems and makes the WoT more secure and universal. Altogether, these trends and demands, indeed, responses to the WoT current problems as well as set the stage for further, more enhanced evolution of technologies there.</p>
+        <div style={{ textAlign: "center" }}>
+          <img src={pic12} alt="IoT in the era of Generative AI" style={{width: "100%", maxWidth: "400px"}} />
+          <p>Synergizing Generative AI and IoT Devices</p>
+          <p></p>
+       
+       </div>
+       </>
+      )
+      
     },
     future: {
       title: "Future Research Directions",
-      content: "In the future, there are great advancements that are expected to take place in the actual domain of the Web of Things (WoT), with proposed solutions aimed to solve some of the emerging and challenging issues and to improve the applicability as well as security of the technology. Advanced Security Mechanisms is an important field as new solutions have to be developed in order to enhance security of IoT networks in regard to new threats that appear from time to time. It comprises the creation of a much more advanced approach to utilize cryptographic procedures as well as utilization of intrusion detection_Table 4_ that meets IoT environment requirements. At the same time, the shift towards Autonomous Systems is pronounced as a shift in paradigm in research: it focuses on developing systems that will require limited super-vision from people, and thus, contribute to reducing human factor as a source of errors. Furthermore, it is accepted that the formulations for the Universal Standards are important to the future of WoT. Formulating informative, international characteristics would guarantee patterns, having obtained devices from different producers, capable of sharing information, making device compatibility and efficiency attainable. Finally, Sustainable Practices are gaining more attention, and research focuses on creating energy efficiency solutions for the growing IoT network. These technologies are not only intended for energy saving of particular devices but also for the integration of renewable energy within them where possible to compensate for negative impacts of the overall IoT applications."
+      content: (
+        <>
+        <p>In the future, there are great advancements that are expected to take place in the actual domain of the Web of Things (WoT), with proposed solutions aimed to solve some of the emerging and challenging issues and to improve the applicability as well as security of the technology. Advanced Security Mechanisms is an important field as new solutions have to be developed in order to enhance security of IoT networks in regard to new threats that appear from time to time. It comprises the creation of a much more advanced approach to utilize cryptographic procedures as well as utilization of intrusion detection that meets IoT environment requirements. At the same time, the shift towards Autonomous Systems is pronounced as a shift in paradigm in research: it focuses on developing systems that will require limited super-vision from people, and thus, contribute to reducing human factor as a source of errors. Furthermore, it is accepted that the formulations for the Universal Standards are important to the future of WoT. Formulating informative, international characteristics would guarantee patterns, having obtained devices from different producers, capable of sharing information, making device compatibility and efficiency attainable. Finally, Sustainable Practices are gaining more attention, and research focuses on creating energy efficiency solutions for the growing IoT network. These technologies are not only intended for energy saving of particular devices but also for the integration of renewable energy within them where possible to compensate for negative impacts of the overall IoT applications.</p>
+        <div style={{ textAlign: "center" }}>
+          <img src={pic11} alt="IoT Overview" style={{width: "100%", maxWidth: "400px"}} />
+          <p>Image shows every device Seamlessly Integrated through the Internet of Things (IoT)</p>
+       
+       </div> 
+       </>
+      
+      )
     }
   };
 
@@ -26,7 +70,7 @@ function Home() {
     <div className="home">
       <div className="content">
         <h2>{sections[activeSection].title}</h2>
-        <p>{sections[activeSection].content}</p>
+        {sections[activeSection].content}
       </div>
       <div className="sidebar"> 
         <h2>Overview</h2>
--- src/Containers/Team.js
+++ src/Containers/Team.js
@@ -1,28 +1,35 @@
 import React, { useState } from 'react';
-import pic1 from "./1.png";
+import '../styles/Team.css'; 
 import pic from "./Nagendra.jpeg";
 import pic2 from "./Gayatri.PNG"
 import pic3 from "./Abdul3.jpeg";
 import pic4 from "./Shruthi.jpeg";
-
+import pic5 from "./sivalakshmi.jpeg"
 
 const teamMembers = [
-  { name: "Venkata Satya S Kumar", role: "Web Developer", bio: "Nagendra has experience in managing software development projects.", photo: pic },
-  { name: "Abdul", role: "Content Validator", bio: "Abdul specializes in Validating the Content in the webPage.", photo: pic3 },
-  { name: "Gayatri", role: "Web Developer", bio: "Gayatri is expert in user interface design and user experience optimization.", photo: pic2 },
-  { name: "Shruthi", role: "Reporting and Delivery Lead", bio: "Shruthi Manages the team Deliverables with in Time", photo: pic4 },
-  { name: "Sivalakshmi", role: "Content Validator", bio: "Sivalakshmi ensures the content of webpage is Upto mark.", photo: pic1 }
+  { name: "Venkata Satya S Kumar", role: "Web Developer", bio: "Nagendra has experience in managing software development projects.", photo: pic, contributions: ["Nagendra sucessfully managed the front-end development of the website, delivering a well-organized framework. He also contributed in LaTeX template and conducted a thorough literature review in  template. In addition, he prepared a taxonomy table for our research and carefully picked publications that were closely connected with our survey issue, furthering the depth and usefulness of our study."] },
+  { name: "Abdul", role: "Content Validator", bio: "Abdul specializes in Validating the Content in the webPage.", photo: pic3, contributions: ["Abdul was responsible for citing template references and selecting the relevant papers, ensuring that all sources were accurately acknowledged and aligned with the project's objectives."] },
+  { name: "Gayatri", role: "Web Developer", bio: "Gayatri is expert in user interface design and user experience optimization.", photo: pic2, contributions: ["Gayatri was responsible for creating the LaTeX template's abstract and introduction, as well as ensuring that all references were cited properly mentioned. In addition, she collaborated with Nagendra to successfully implement the project, providing important perspectives to the team."] },
+  { name: "Shruthi", role: "Reporting and Delivery Lead", bio: "Shruthi Manages the team Deliverables within Time", photo: pic4, contributions: ["Shruthi was responsible for selecting the research papers for the project, focusing specifically on those that closely aligned with our survey topic."] },
+  { name: "Sivalakshmi", role: "Content Validator", bio: "Sivalakshmi ensures the content of webpage is Upto mark.", photo: pic5, contributions: ["Sivalakshmi was responsible for choosing the research papers for the project, selecting those that best matched our survey theme to enrich our study and findings."] }
 ];
 
 function Team() {
   const [selectedMember, setSelectedMember] = useState(null);
+  const [showContributions, setShowContributions] = useState(false);
 
   const handleMemberClick = member => {
     setSelectedMember(member);
+    setShowContributions(false); // Reset to hide contributions initially
+  };
+
+  const toggleContributions = () => {
+    setShowContributions(!showContributions);
   };
 
   const closeModal = () => {
     setSelectedMember(null);
+    setShowContributions(false); // Reset on modal close
   };
 
   return (
@@ -42,6 +49,10 @@ function Team() {
             <span className="close-button" onClick={closeModal}>&times;</span>
             <h2>{selectedMember.name} - {selectedMember.role}</h2>
             <p>{selectedMember.bio}</p>
+            <button onClick={toggleContributions}>Work Contribution</button>
+            {showContributions && (
+              <p>{selectedMember.contributions.join(' ')}</p> // Join contributions into a single paragraph
+            )}
           </div>
         </div>
       )}
